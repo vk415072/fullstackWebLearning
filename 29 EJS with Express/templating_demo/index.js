fbff14f0bec469cebbf40a755c2c7fa2c7000c7b
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-05-09 11:38:48 +0530

    ejs setup and first use

--- 29 EJS with Express/templating_demo/index.js
+++ 29 EJS with Express/templating_demo/index.js
@@ -1,8 +1,20 @@
 const express = require('express');
 const app = express();
+const path = require('path');
+
+// telling my app to use EJS
+app.set('view engine','ejs');
+// also create a folder named "views" in current working directory.
+// and create a file "home.ejs"
+
+// now taking the current dir name and joining it to /views. as the /views will not work if i run index.js from a diff. path.
+app.set('views', path.join(__dirname, '/views'))
 
 app.get('/', (req, res) =>{
-    res.send('Hi!');
+    // res.send('Hi!');
+
+    //sending home.ejs using render (no need to specify like views/home.ejs)
+    res.render('home.ejs');
 })
 
 
