a126e95e3858886e3b571ab86db01b92a08c4e79
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-05-09 21:13:49 +0530

    CRUD demo ini

---
+++ 30 RESTful Routes/REST_demo/index.js
@@ -0,0 +1,68 @@
+const { json } = require('express');
+const express = require('express');
+const app = express();
+// 7. require path to join views ejs
+const path = require('path');
+
+
+// 3. so to parse the body data type
+app.use(express.urlencoded({ ecyended: true }));
+// 5. similarly to parse the jason data:
+app.use(express.json());
+
+// 8. joining path of views ejs
+app.set('views', path.join(__dirname, 'views')); 
+
+// 6. now setting view engine for ejs
+app.set('view engine', 'ejs')
+
+
+
+// 9. making comments db
+const comments = [
+    {
+        username: 'Tod',
+        comment: 'lol, that is so funny!'
+    },
+    {
+        username: 'Vivek',
+        comment: 'I like to go to movies with my friends'
+    },
+    {
+        username: 'Shiv',
+        comment: 'Plz delete your account, Tod'
+    },
+    {
+        username: 'Elsa',
+        comment: 'Woof, woof, woof!!!'
+    },
+    {
+        username: 'Mike',
+        comment: 'you owe me 10 bucks!'
+    }
+]
+
+// 10. render all comments to ejs
+app.get('/comments', (req, res) => {
+    res.render('comments/index.ejs', {comments})
+})
+
+
+app.get('/tacos', (req, res) => {
+    res.send('GET /tacos response');
+})
+
+app.post('/tacos', (req, res) => {
+    // 1. but in post, we can recieve a body
+    console.log(req.body)
+    // 2. this will result in undefined as we have not defined which type of body we want to recieve.
+
+    // 4. extracting the body data
+    const { meat, qty } = req.body;
+    res.send(`OK, here are your ${qty} ${meat} Taco!`)
+})
+
+
+app.listen(3000, () => {
+    console.log('listening to port 3000');
+})
