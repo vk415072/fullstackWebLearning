19ab9b9255601d05bb185952b5a575e58671a54c
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-05-09 11:50:38 +0530

    passing data to templates

--- 29 EJS with Express/templating_demo/index.js
+++ 29 EJS with Express/templating_demo/index.js
@@ -3,19 +3,28 @@ const app = express();
 const path = require('path');
 
 // telling my app to use EJS
-app.set('view engine','ejs');
+app.set('view engine', 'ejs');
 // also create a folder named "views" in current working directory.
 // and create a file "home.ejs"
 
 // now taking the current dir name and joining it to /views. as the /views will not work if i run index.js from a diff. path.
 app.set('views', path.join(__dirname, '/views'))
 
-app.get('/', (req, res) =>{
+app.get('/', (req, res) => {
     // res.send('Hi!');
-
     //sending home.ejs using render (no need to specify like views/home.ejs)
     res.render('home.ejs');
-})
+});
+
+
+//creating another ejs file, generating random no. here and passing it to the ejs file.
+app.get('/rand', (req, res) =>{
+    const num = Math.floor(Math.random()*10) + 1;
+    // num will be passed under the name "rand" to my random.ejs file
+    res.render('random.ejs', {rand:num}); 
+    // or i can just pass {num} directly and use same in ejs file
+});
+
 
 
 app.listen(3000, () => {
