594b2a65d3875ede77aff567db97b9a32b3ae2fb
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-05-11 11:52:28 +0530

    pending...

--- 31 Mongoose Basics/mongoose_basics/index.js
+++ 31 Mongoose Basics/mongoose_basics/index.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
-// deafult mogodb URI
-// "/movieApp" is the database custom name
+// 1. deafult mogodb URI
+// 2. "/movieApp" is the database custom name
 mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log("connection open!")
@@ -10,10 +10,27 @@ mongoose.connect('mongodb://localhost:27017/movieApp', { useNewUrlParser: true, 
         console.log(err);
     });
 
-// // copies from mongoose docs
+// // 3. copies from mongoose docs
 // const db = mongoose.connection;
 // db.on('error', console.error.bind(console, 'connection error'));
 // db.once('open', function(){
-//     // we are connected!
+//     // 4. we are connected!
 //     console.log("connection open!");
 // });
+
+
+
+// 5. defining new movies databse
+// 6. first have to defne its Schema
+const moviesSchema = new mongoose.Schema({
+    tittle: String,
+    year: Number,
+    rating: String,
+    score: Number
+})
+
+// now i've to make a model using this schema
+const movie =  mongoose.model('Movie', moviesSchema)
+// now i can make instances and save em to mongo db
+const iron_man = new movie({title: 'Iron Man', year: 2004, score: 9.2, rating: 'R'});
+iron_man.save();
