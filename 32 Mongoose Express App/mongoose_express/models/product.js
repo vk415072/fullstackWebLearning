2d2ad300e96558aeaa618b34023e016872ec2836
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-05-11 14:20:46 +0530

    created product db schema

---
+++ 32 Mongoose Express App/mongoose_express/models/product.js
@@ -0,0 +1,29 @@
+// 1. requiring mongoose
+const mongoose = require('mongoose');
+
+
+
+// 2. making Schema
+const productSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true,
+        min: 0
+    },
+    category: {
+        type: String,
+        lowercase: true,
+        enum: ['fruit', 'vegetable', 'dairy']
+    }
+})
+
+
+// 3. compiling our model
+const Product = mongoose.model('Product', productSchema);
+
+// 4. exporting that from this file to use somewhere else
+module.exports = Product;
