3b5c825fb36fe2973a67bc2c2db765a63d0a9799
Vivek Sengar <36097100+vk415072@users.noreply.github.com>
2021-05-13 12:00:02 +0530

    using middleware in routes

--- 33 Middleware in Express/middleware_intro/index.js
+++ 33 Middleware in Express/middleware_intro/index.js
@@ -17,22 +17,36 @@ const morgan = require("morgan");
 app.use(morgan("tiny"));
 // 8. now this will use as a middleware if i go to any url. Also it will log the info.
 
-// 9. using next() to continue the chain of middleware.
-// 10. sp whatever i enter in url, these will execute.
+// // 9. using next() to continue the chain of middleware.
+// // 10. sp whatever i enter in url, these will execute.
+// app.use((req, res, next) => {
+//   console.log("This is my 1st middleware!");
+//   next();
+//   // 11. this will still execute but after all the chained middlewares
+//   // 12. i can stop the execution after "next()"" if i add "return" before it
+//   console.log("This is my 1st middleware - after calling next!");
+// });
+// app.use((req, res, next) => {
+//   console.log("This is my 2nd middleware!");
+//   next();
+// });
+
+// 12. adding middleware function
+// 13. we're just recreating what morgan was doing
 app.use((req, res, next) => {
-  console.log("This is my 1st middleware!");
-  next();
-  // 11. this will still execute but after all the chained middlewares
-  // 12. i can stop the execution after "next()"" if i add "return" before it
-  console.log("This is my 1st middleware - after calling next!");
-});
-app.use((req, res, next) => {
-  console.log("This is my 2nd middleware!");
+  console.log(req.method, req.path);
+  // 14. i can change the type of req to for eg: GET
+  //   req.method = 'GET'
+  // 15. now doing something to the req in the middle
+  // 16. for eg: adding time
+  req.requestTime = Date.now();
   next();
 });
 
 // 2. testing
 app.get("/", (req, res) => {
+    // 17. using the requestTime from middleware
+    console.log(`REQUEST DATE: ${req.requestTime}`);
   res.send("Home Page");
 });
 
